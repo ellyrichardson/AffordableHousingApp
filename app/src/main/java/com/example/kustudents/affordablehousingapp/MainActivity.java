62446073cc72316e708cbda074a05604b9a34062
Elly Richardson <elly.richardson18@gmail.com>
2019-09-21 23:12:11 -0500

    Added the Search Filters page and added some code to it

--- app/src/main/java/com/example/kustudents/affordablehousingapp/MainActivity.java
+++ app/src/main/java/com/example/kustudents/affordablehousingapp/MainActivity.java
@@ -3,11 +3,29 @@ package com.example.kustudents.affordablehousingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button searchFilterBtn;
+    EditText searchAttributeEditTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchFilterBtn = (Button) findViewById(R.id.searchFilterButton);
+        searchAttributeEditTxt = (EditText) findViewById(R.id.searchAttributeEditText);
+
+        searchFilterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchFilter = searchAttributeEditTxt.getText().toString();
+                
+            }
+        });
     }
 }
