b11387faf14afdc5bf2f78558a1dc9bad7ff50a8
Elly Richardson <elly.richardson18@gmail.com>
2019-10-11 02:25:50 -0500

    Added some fragment codes, but app crashes when activity with fragment was accessed.

--- app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/ResultsActivity.java
+++ app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/ResultsActivity.java
@@ -3,6 +3,8 @@ package com.example.kustudents.affordablehousingapp._activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.kustudents.affordablehousingapp.R;
 import com.example.kustudents.affordablehousingapp._helpers.SearchFilterTransport;
@@ -10,12 +12,15 @@ import com.example.kustudents.affordablehousingapp._helpers.SearchFilterTranspor
 public class ResultsActivity extends AppCompatActivity {
 
     SearchFilterTransport searchFilterTransport;
+    Button heatMapButton;
     static String apiURL = "http://192.168.1.2:8000/api/search/?search=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        heatMapButton = (Button) findViewById(R.id.resultsHeatMapButton);
 
         // Gets the searchedFilter object passed from MainActivity
         Intent intent = getIntent();
@@ -25,5 +30,14 @@ public class ResultsActivity extends AppCompatActivity {
 
         // Executes the AsyncTask for HTTPUrlConnections with the searchFilter
         searchFilterTransport.execute(apiURL, searchFilter);
+
+        heatMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ResultsActivity.this, HeatMapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
