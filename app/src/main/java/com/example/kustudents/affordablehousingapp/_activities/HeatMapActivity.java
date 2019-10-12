6794dbc48bbf575cd3a06555c55582525097802b
Elly Richardson <elly.richardson18@gmail.com>
2019-10-12 01:07:08 -0500

    Added code for Google Maps fragment. Some issues like API key not existing has to be fixed. It also currently only shows Sydney, Australia

--- app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/HeatMapActivity.java
+++ app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/HeatMapActivity.java
@@ -6,14 +6,34 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.kustudents.affordablehousingapp.R;
+import com.example.kustudents.affordablehousingapp._fragments.HeatMapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class HeatMapActivity extends FragmentActivity {
+public class HeatMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heat_map);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.hmHeatMapFragment);
+        mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // This void function has to be overriden under OnMapReadyCallBack so that getMapAsync() can work in this activity.
+
+        // Adding a marker, currently Sydney, Australia
+        // And move the map's camera to the same location
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+            .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
