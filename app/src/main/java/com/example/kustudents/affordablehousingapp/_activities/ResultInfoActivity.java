f803ee65d0597fce2a970d8fd4bc5d1022dcdd44
Elly Richardson <elly.richardson18@gmail.com>
2019-10-18 01:08:42 -0500

    Google maps for the individual housing results can now be shown!

--- app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/ResultInfoActivity.java
+++ app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/ResultInfoActivity.java
@@ -6,10 +6,16 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.kustudents.affordablehousingapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
 
-public class ResultInfoActivity extends AppCompatActivity {
+public class ResultInfoActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     TextView developmentName, inspScore, city, state, zip;
 
@@ -30,7 +36,33 @@ public class ResultInfoActivity extends AppCompatActivity {
         inspScore.setText((String)intent.getSerializableExtra("inspectionScore"));
         city.setText((String)intent.getSerializableExtra("city"));
         state.setText((String)intent.getSerializableExtra("state"));
-        //zip.setText("Housing Zip: " + (String)intent.getSerializableExtra("zip"));
+        zip.setText("66222");
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.resultsHeatMapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // This void function has to be overriden under OnMapReadyCallBack so that getMapAsync() can work in this activity.
+        LatLng housingMarker = new LatLng(getHousingLat(), getHousingLong());
+        googleMap.addMarker(new MarkerOptions().position(housingMarker)
+                .title(getHousingDevelopmentName()));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(housingMarker, 14.0f));
+    }
+
+    public float getHousingLat() {
+        Intent intent = getIntent();
+        return Float.parseFloat((String)intent.getSerializableExtra("latitude"));
+    }
+
+    public float getHousingLong() {
+        Intent intent = getIntent();
+        return Float.parseFloat((String)intent.getSerializableExtra("longitude"));
+    }
+
+    public String getHousingDevelopmentName() {
+        Intent intent = getIntent();
+        return (String)intent.getSerializableExtra("developmentName");
     }
 }
