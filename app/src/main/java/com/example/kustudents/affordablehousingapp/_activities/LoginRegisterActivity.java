c6cec13c30eddc1f3c8a99370cee1af08cba5bf4
Elly Richardson <elly.richardson18@gmail.com>
2019-11-10 21:53:05 -0600

    Added login and register semi functionality

--- app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/LoginRegisterActivity.java
+++ app/src/main/java/com/example/kustudents/affordablehousingapp/_activities/LoginRegisterActivity.java
@@ -1,15 +1,39 @@
 package com.example.kustudents.affordablehousingapp._activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.kustudents.affordablehousingapp.R;
 
 public class LoginRegisterActivity extends AppCompatActivity {
 
+    Button loginButton, registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+
+        loginButton = (Button) findViewById(R.id.logRegLoginButton);
+        registerButton = (Button) findViewById(R.id.logRegRegisterButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginRegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginRegisterActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
